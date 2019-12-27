e3177054f06c604a16e535c0f8c6a0d2bbe73465
caili <14091586@qq.com>
2019-12-27 16:43:08 +0800

    first

---
+++ djs/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from djs.models import Post
+#admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display=('title','slug','pub_date')
+
+admin.site.register(Post,PostAdmin)
