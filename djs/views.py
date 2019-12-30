3ebd8ea34fa92f5e4c97ed4659eeb396d5522d0a
caili <14091586@qq.com>
2019-12-30 14:09:50 +0800

    1230

--- djs/views.py
+++ djs/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from djs.models import Post
+from datetime import datetime
+# def homepage(request):
+#     posts = Post.objects.all()
+#     post_lists=list()
+#     for count, post in enumerate(posts):
+#         post_lists.append('No.{}:'.format(str(count))+str(post)+'<br>')
+#         post_lists.append('<small>'+str(post.body.encode('utf-8'))+'</small><br><hr>')
+#     return HttpResponse(post_lists)
+
+def homepage(request):
+    posts=Post.objects.all()
+    now = datetime.now()
+    return render(request,'index.html',locals())
+
+def showpost(request,slug):
+    try:
+        post=Post.objects.get(slug=slug)
+        if post!=None:
+            return render(request,'post.html',locals())
+    except:
+        return redirect('/')
